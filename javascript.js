2ec8dce9f6873013ecbd992c7123fb037b5653b2
Nishivaly <51959902+Nishivaly@users.noreply.github.com>
2025-01-17 13:30:54 -0300

    Add square bg color change on mouse enter

--- javascript.js
+++ javascript.js
@@ -2,6 +2,12 @@ generateGrid(16);
 
 function generateGrid(size) {
     const container = document.querySelector('.container');
+    container.addEventListener('mouseenter', event => {
+        if (event.target.classList.contains('square')) {
+            event.target.style.backgroundColor = 'blue';
+
+        };
+    }, true);
 
     const square = document.createElement('div');
     square.setAttribute('class', 'square');
