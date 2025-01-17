8ba21befb6b94d10843e52ab3c73ad68cda69fca
Nishivaly <51959902+Nishivaly@users.noreply.github.com>
2025-01-17 12:18:51 -0300

    Clean up and reorder

--- javascript.js
+++ javascript.js
@@ -1,13 +1,14 @@
-const container = document.querySelector('.container');
-const square = document.createElement('div');
-square.setAttribute('class', 'square');
-
-const row = document.createElement('div');
-row.setAttribute('class', 'row');
-
-generateGrid(30);
+generateGrid(16);
 
 function generateGrid(size) {
+    const container = document.querySelector('.container');
+
+    const square = document.createElement('div');
+    square.setAttribute('class', 'square');
+
+    const row = document.createElement('div');
+    row.setAttribute('class', 'row');
+
     for (let i = 0; i < size; i++) {
         const cloneRow = row.cloneNode();
         container.appendChild(cloneRow);
@@ -15,6 +16,6 @@ function generateGrid(size) {
         for (let j = 0; j < size; j++) {
             const cloneSquare = square.cloneNode();
             cloneRow.appendChild(cloneSquare);
-        }
-    }
-}
+        };
+    };
+};
