b8706f4584c24c677a8d5594559692e6d54707fb
Nishivaly <51959902+Nishivaly@users.noreply.github.com>
2025-01-17 11:37:24 -0300

    Add function to populate container with rows and squares

--- javascript.js
+++ javascript.js
@@ -2,8 +2,19 @@ const container = document.querySelector('.container');
 const square = document.createElement('div');
 square.setAttribute('class', 'square');
 
+const row = document.createElement('div');
+row.setAttribute('class', 'row');
 
-for (let i = 0; i < 16; i++) {
-    const cloneSquare = square.cloneNode();
-    container.appendChild(cloneSquare);
+generateGrid(16);
+
+function generateGrid(size) {
+    for (let i = 0; i < size; i++) {
+        const cloneRow = row.cloneNode();
+        container.appendChild(cloneRow);
+
+        for (let j = 0; j < size; j++) {
+            const cloneSquare = square.cloneNode();
+            cloneRow.appendChild(cloneSquare);
+        }
+    }
 }
